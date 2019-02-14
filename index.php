706bde193f6b503c44d6e5dad06d49969d7f8dec
Colm Woodlock <Colm Woodlock>
2019-02-14 13:06:41 +0000

    Restarting login system

--- index.php
+++ index.php
@@ -1,37 +1,28 @@
-<!DOCTYPE html>
+<?php
+include('login.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+header("location: profile.php"); // Redirecting To Profile Page
+}
+?> 
 
+<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>Ajax database call test</title>
-        <script>
-            //Adapted from https://www.w3schools.com/php/php_ajax_database.asp
-            //test
-            function showUser(str) {
-                if (str == "") {
-                    document.getElementById("txtHint").innerHTML = "";
-                    return;
-                } else {
-                    if (window.XMLHttpRequest) {
-                        // code for IE7+, Firefox, Chrome, Opera, Safari
-                        xmlhttp = new XMLHttpRequest();
-                    } else {
-                        // code for IE6, IE5
-                        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-                    }
-                    xmlhttp.onreadystatechange = function () {
-                        if (this.readyState == 4 && this.status == 200) {
-                            document.getElementById("txtHint").innerHTML = this.responseText;
-                        }
-                    };
-                    xmlhttp.open("POST", "SqlQuery.php?q=" + str, true);
-                    xmlhttp.send();
-                }
-            }
-        </script>
+      <title>Login Form in PHP with Session</title>
+      <link href="style.css" rel="stylesheet" type="text/css">
     </head>
+
     <body>
-        <input type="submit" class="button" name="display" value="Click here" onclick="showUser(this.value)"/>
-        <div id="txtHint"><h2>The data from a row in the database will be displayed here...</h2></div>
+        <div id="login">
+            <h2>Login Form</h2>
+            <form action="" method="post">
+                <label>UserName :</label>
+                <input id="name" name="username" placeholder="username" type="text">
+                <label>Password :</label>
+                <input id="password" name="password" placeholder="**********" type="password"><br><br>
+                <input name="submit" type="submit" value=" Login ">
+                <span><?php echo $error; ?></span>
+            </form>
+        </div>
     </body>
 </html>
