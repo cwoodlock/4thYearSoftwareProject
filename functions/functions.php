c6923111e28d8ba33f9f54ce533642de6343494c
Colm Woodlock <Colm Woodlock>
2019-02-21 13:10:55 +0000

    Finished user validation

--- functions/functions.php
+++ functions/functions.php
@@ -2,7 +2,7 @@
 
 function clean($clean){ //Ensure user input data does not include any wildcards
 
-	return htmlentities($string);
+	return htmlentities($clean);
 
 }
 
@@ -39,6 +39,118 @@ function token_generator(){
 	$token = $_SESSION['token'] = md5(uniqid(mt_rand(), true));
 
 	return token;
+}
+
+function validation_error($error){
+
+	echo '
+			<div class="alert alert-danger" role="alert">
+  				'.$error .' 
+			</div>
+		 ';
+
+}
+
+function email_exists($email){ //checks if email was used before
+
+	$sql = "SELECT id FROM users WHERE email = '$email'";
+
+	$result = query($sql);
+
+	if(row_count($result) == 1){
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+function username_exists($username){ //checks if username was used before
+
+	$sql = "SELECT id FROM users WHERE username = '$username'";
+
+	$result = query($sql);
+
+	if(row_count($result) == 1){
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+function validate_user_registration(){ //Function will validate the user
+
+	$errors = [];
+
+	$min = 3;
+	$max = 20;
+
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+		$first_name = clean($_POST['first_name']);
+		$last_name = clean($_POST['last_name']);
+		$username = clean($_POST['username']);
+		$email = clean($_POST['email']);
+		$password = clean($_POST['password']);
+		$confirm_password = clean($_POST['confirm_password']);
+
+		if(strlen($first_name) < 3){
+			
+			$errors[] = "Your first name cannot be less than {$min} characters";
+		}
+
+		if(strlen($last_name) < 3){
+			
+			$errors[] = "Your last name cannot be less than {$min} characters";
+		}
+
+		if(strlen($username) < 3){
+			
+			$errors[] = "Your username cannot be less than {$min} characters";
+		}
+
+		if(strlen($first_name) > 20){
+			
+			$errors[] = "Your first name cannot be more than {$max} characters";
+		}
+
+		if(strlen($last_name) > 20){
+			
+			$errors[] = "Your last name cannot be more than {$max} characters";
+		}
+
+		if(strlen($username) > 20){
+			
+			$errors[] = "Your username cannot be more than {$max} characters";
+		}
+
+		if($password !== $confirm_password) {
+			$errors[] = "Your passwords do not match";
+		}
+
+		if(email_exists($email)){
+
+			$errors[] = "{$email} is already registered";
+
+		}
+
+		if(username_exists($username)){
+
+			$errors[] = "{$username} is already registered";
+
+		}
+
+		if(!empty($errors)){
+
+			foreach ($errors as $error) {
+
+				//Display validation error
+				echo validation_error($error);			
+			}
+		}
+
+	}
 }
 
 ?>
