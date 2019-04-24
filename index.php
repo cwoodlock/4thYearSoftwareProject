b9b1f0d5e9a9cd1df5dd5b35dee580be70318b7e
cwoodlock <g00341460@gmit.ie>
2019-04-24 18:26:48 +0100

    Added modal for placing the bets

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
   </div>
 
   <?php 
-			  		$sql = "SELECT team1, team2, odds_team1, odds_team2, lay_team1, lay_team2 FROM contest";
+  	if(logged_in()){
+			  		$sql = "SELECT contestID, team1, team2, odds_team1, odds_team2, lay_team1, lay_team2, odds_draw, lay_draw FROM contest";
 
 			  		$result = query($sql);
 			  		confirm($result);
@@ -43,16 +44,146 @@
 			       									<td>'.$row['lay_team2'].'</td>
 			       								</tr>
 
+			       								
+
 		       								</table>
-		       								<a class="btn btn-success btn-lg" href="place_bet.php" role="button">Place Bet</a>
+		       								<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+												<!-- Modal -->
+												<div id="myModal" class="modal fade" role="dialog">
+												  <div class="modal-dialog">
+
+												    <!-- Modal content-->
+												    <div class="modal-content">
+												      <div class="modal-header">
+												        <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+												        <h4 class="modal-title">'.$row['team1'].' Vs. '.$row['team2'].'</h4>
+												      </div>
+
+												      <div class="modal-body">
+												        <div class = "panel-body" >
+														   	<div>
+														      	'.$row['team1'].'
+															      	<p>
+																	  	<a class="btn btn-primary" data-toggle="collapse" href="#team1bet" role="button" aria-expanded="false" aria-controls="team1bet">
+																	    '.$row['odds_team1'].'
+																	  	</a>
+
+																	  	<button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#team1lay" aria-expanded="false" aria-controls="team1lay">
+																	    '.$row['lay_team1'].'
+																	  	</button>
+																	</p>
+																	<div class="collapse" id="team1bet">
+																	  <div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Bet Odds:
+																			  	<input class="form-control" type="number" id="example-number-input" value="'.$row['odds_team1'].'">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	  </div>
+																	</div>
+																<div class="collapse" id="team1lay">
+																	<div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Lay Odds:
+																			  	<input class="form-control" type="number" id="example-number-input">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	</div>
+																</div>
+															</div>
+
+
+														    <div>
+														      	Draw
+															      	<p>
+																	  	<a class="btn btn-primary" data-toggle="collapse" href="#drawbet" role="button" aria-expanded="false" aria-controls="drawbet">
+																	    '.$row['odds_draw'].'
+																	  	</a>
+
+																	  	<button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#drawlay" aria-expanded="false" aria-controls="drawlay">
+																	   '.$row['lay_draw'].'
+																	  	</button>
+																	</p>
+																	<div class="collapse" id="drawbet">
+																	  <div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Bet Odds:
+																			  	<input class="form-control" type="number" id="example-number-input" value="'.$row['lay_draw'].'">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	  </div>
+																	</div>
+																<div class="collapse" id="drawlay">
+																	<div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Lay Odds:
+																			  	<input class="form-control" type="number" id="example-number-input">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	</div>
+																</div>
+															</div>	
+
+
+														    <div>
+														      	'.$row['team2'].'
+															      	<p>
+																	  	<a class="btn btn-primary" data-toggle="collapse" href="#team2bet" role="button" aria-expanded="false" aria-controls="team2bet">
+																	    '.$row['odds_team2'].'
+																	  	</a>
+
+																	  	<button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#team2lay" aria-expanded="false" aria-controls="team2lay">
+																	    '.$row['lay_team2'].'
+																	  	</button>
+																	</p>
+																	<div class="collapse" id="team2bet">
+																	  <div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Bet Odds:
+																			  	<input class="form-control" type="number" id="example-number-input" value="'.$row['odds_team2'].'">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	  </div>
+																	</div>
+																<div class="collapse" id="team2lay">
+																	<div class="card card-body">
+																	    <div class="jumbotron" style = "padding: 5px 5px 5px;">
+																			<p class="lead">
+																				Lay Odds:
+																			  	<input class="form-control" type="number" id="example-number-input">
+																			    <a class="btn btn-success btn-lg" href="#" role="button">Place Bet</a>
+																			</p>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														      
+														   </div>
+														</div>
+												      </div>
+
+												      
+												    </div>
+
+												  </div>
+												</div>
 	       								</div>
        								</div>
        								</div>';
-			  				
-							
-
-
-
+       							
+       							}
 			  			}
 			  		}
 			  	?>
