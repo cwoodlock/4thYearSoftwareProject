7b57a3ee18b630aad07cd6336d9db6b2986b8d2e
cwoodlock <g00341460@gmit.ie>
2019-04-26 23:30:35 +0100

    Displaying users bets on profile page

--- admin.php
+++ admin.php
@@ -3,18 +3,48 @@
   <?php include("includes/nav.php") ?>
 
 
-	<div class="jumbotron">
-		<h1 class="text-center">
+	<div class = "panel panel-success">
+		<h4 class="text-center">
 			<?php 
 				if(logged_in()){
-					echo "LOGGED IN";
+			  		$sql = "SELECT memberbets.betName, memberbets.betAmount, memberbets.betOdds
+								FROM memberbets
+  								JOIN users ON memberbets.usersID = users.id
+								WHERE memberbets.usersID = users.id";
+
+			  		$result = query($sql);
+			  		confirm($result);
+
+			  		if($result-> num_rows > 0){
+			  			while ($row = $result-> fetch_assoc()) {
+			  				echo '<table class="table">
+									  <thead>
+									    <tr>
+									      <th scope="col"></th>
+									      <th scope="col">Bet Name</th>
+									      <th scope="col">Bet Amount</th>
+									      <th scope="col">Bet Odds</th>
+									    </tr>
+									  </thead>
+									  <tbody>
+									    <tr>
+									  	  <th scope="col"></th>
+									      <td>'.$row['betName'].'</td>
+									      <td>'.$row['betAmount'].'</td>
+									      <td>'.$row['betOdds'].'</td>
+									      
+									    </tr>
+									  </tbody>
+									</table>';
+								}
+					}
 				} else {
 					redirect("index.php");
 				}
 
 			?>
 
-		</h1>
+		</h4>
 	</div>
 
 <?php include("includes/footer.php") ?>
--- index.php
+++ index.php
@@ -46,26 +46,27 @@
 									          <div class= "panel-heading"><h3   class="panel-title">'.$row['team1'].' Vs '.$row['team2'].'</h3></div>
 
 									          <div class= "body-panel">
-									            <table>
+									            <table class="table table-dark">
+									            <thread>
 									              <tr>
-									                <th>Team 1</th>
-									                <th>Team 2</th>
+									                <th scope="col">Team 1</th>
+									                <th scope="col">Team 2</th>
 									              </tr>
 									              <tr>
 									                <td>'.$row['team1'].'</td>
 									                <td>'.$row['team2'].'</td>
 									              </tr>
 									              <tr>
-									                <td>'.$row['odds_team1'].'</td>
-									                <td>'.$row['odds_team2'].'</td>
+									                <td>Bet Odds: '.$row['odds_team1'].'</td>
+									                <td>Bet Odds:'.$row['odds_team2'].'</td>
 									              </tr>
 									              <tr>
-									                <td>'.$row['lay_team1'].'</td>
-									                <td>'.$row['lay_team2'].'</td>
+									                <td>Lay Odds:'.$row['lay_team1'].'</td>
+									                <td>Lay Odds:'.$row['lay_team2'].'</td>
 									              </tr>
 
 									              
-
+									            </thred>
 									            </table>
 									            <button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Place Bet/Lay</button>
 
